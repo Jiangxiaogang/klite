f4f8aa24497780dbf07d77cc425192d1565e7762
jiangxiaogang <kerndev@foxmail.com>
2017-07-20 21:14:05 +0800

    使用单线程方式重写软件定时器.

--- sources/extend/timer.c
+++ sources/extend/timer.c
@@ -1,71 +1,108 @@
-/* 
+/****************************************************************************
 * 描述: 软件定时器
 *       可简化定时任务的代码编写
 * 蒋晓岗<kerndev@foxmail.com>
-*/
+* 2017.7.20
+****************************************************************************/
 #include "kernel.h"
+#include "list.h"
 #include "timer.h"
 
 struct timer
 {
+	struct timer* prev;
+	struct timer* next;
 	int id;
-	int elapse;
+	int counter;
+	int reload;
 	void(*callback)(int);
-	kmutex_t mutex;
-	kthread_t thread;
 };
 
-static uint32_t timer_stk_size;
-static uint32_t timer_thread_prio;
+struct timer_list
+{
+	struct timer* head;
+	struct timer* tail;
+	kmutex_t mutex;
+	kevent_t event;
+};
+
+static struct timer_list list;
+
+//告诉你，时间已经过了tick这么久了!!!!
+//你给我好好检查一下定时器, 把时间到了的都给我处理了!!!!
+//然后再告诉我距离下一个定时器发作还有多久!!!!
+static int timer_timetick(int tick)
+{
+	int min;
+	struct timer* node;
+	min = 1000;
+	kmutex_lock(list.mutex);
+	for(node=list.head; node!=NULL; node=node->next)
+	{
+		if(node->counter > 0)
+		{
+			node->counter -= tick;
+			if(node->counter <= 0)
+			{
+				node->counter = node->reload;
+				node->callback(node->id);
+			}
+		}
+		else
+		{
+			node->counter = node->reload;
+		}
+		if(node->counter < min)
+		{
+			min = node->counter;
+		}
+	}
+	kmutex_unlock(list.mutex);
+	return min;
+}
 
 //定时器线程
-//循环调用定时器回调函数
+//最多等待下一个定时器发作这么久的时间，就要去检查一下!
 static void timer_thread_entry(void* arg)
 {
-	struct timer* tmr;
-	tmr = (struct timer*)arg;
+	int tick;
+	int timeout;
+	uint32_t tick;
+	timeout = 0;
 	while(1)
 	{
-		kthread_sleep(tmr->elapse);
-		kmutex_lock(tmr->mutex);
-		tmr->callback(tmr->id);
-		kmutex_unlock(tmr->mutex);
+		tick = kernel_tick();
+		kevent_timedwait(list.event, timeout);
+		tick = kernel_tick() - tick;
+		timeout = timer_timetick(tick);
 	}
+}
+
+//创建定时器
+timer_t timer_create(int id, int elapse, void(*callback)(int))
+{
+	struct timer* node;
+	node = kmem_alloc(sizeof(struct timer));
+	if(node != NULL)
+	{
+		node->id = id;
+		node->counter= 0;
+		node->reload = elapse;
+		node->callback = callback;
+		kmutex_lock(list.mutex);
+		list_append(&list, node);
+		kmutex_unlock(list.mutex);
+		kevent_post(list.event);
+	}
+	return node;
 }
 
 //初始化定时器模块
 //设置定时器线程的堆栈大小和线程优先级
 void timer_init(uint32_t stk_size, int prio)
 {
-	timer_stk_size = stk_size;
-	timer_thread_prio = prio;
-}
-
-//创建定时器
-timer_t timer_create(int id, int elapse, void(*callback)(int))
-{
-	struct timer* tmr;
-	tmr = kmem_alloc(sizeof(struct timer));
-	if(tmr != NULL)
-	{
-		tmr->id = id;
-		tmr->elapse = elapse;
-		tmr->callback = callback;
-		tmr->mutex = kmutex_create();
-		tmr->thread = kthread_create(timer_thread_entry, tmr, timer_stk_size);
-		kthread_setprio(tmr->thread, timer_thread_prio);
-	}
-	return tmr;
-}
-
-//销毁定时器
-void timer_destroy(timer_t timer)
-{
-	struct timer* tmr;
-	tmr = (struct timer*)timer;
-	kmutex_lock(tmr->mutex);
-	kthread_destroy(tmr->thread);
-	kmutex_unlock(tmr->mutex);
-	kmutex_destroy(tmr->mutex);
-	kmem_free(tmr);
+	kthread_t thread;
+	list_init(&list);
+	thread = kthread_craete(timer_thread_entry, 0, stk_size);
+	kthread_setprio(thread, prio);
 }
